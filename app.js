71717d1d55adbaa605aadd2d4a60a7d78f2c2521
SyedWaleedAli71 <syedwaleed887@gmail.com>
2026-01-02 01:32:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// echo "# Assignment-21" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/SyedWaleedAli71/Assignment-21.git
+// git push -u origin main
+
+
+// Assignment # 21
+// FUNCTIONS, SWITCH
+// STATEMENTS, WHILE... DO-
+// WHILE LOOPS
+
+// Q.1
+// function power(a, b) {
+//   let result = 1;
+
+//   for (let i = 1; i <= b; i++) {
+//     result = result * a;
+//   }
+
+//   return result;
+// }
+
+// let ans = power(2, 3);
+// document.writeln("Answer: " + ans);
+
+// Q.2
+// function checkLeapYear(year) {
+//   if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     return "Leap Year";
+//   } else {
+//     return "Not a Leap Year";
+//   }
+// }
+
+// let year = +prompt("Enter a year");
+
+// let result = checkLeapYear(year);
+
+// document.writeln(year + " is " + result);
+
+// Q.3
